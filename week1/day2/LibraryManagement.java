35dbdb89544706e507af42412bc965f49d4f22d8
Prasanth M <75491693+Prasanth-25@users.noreply.github.com>
2025-03-29 00:28:42 +0530

    Delete week1 directory

--- week1/day2/LibraryManagement.java
+++
@@ -1,9 +0,0 @@
-package week1.day2;
-
-public class LibraryManagement {
-    public static void main(String[] args) {
-        Library read = new Library();
-        read.addBook("Book1");
-        read.issueBook();
-    }
-}
